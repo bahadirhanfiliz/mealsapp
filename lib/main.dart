2deeec4c906cfee5b51280e0fb08be7764258125
Bahadırhan Filiz <bfiliz14@ku.edu.tr>
2020-03-02 18:43:59 +0300

    removed print statement

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,6 @@ class _MyAppState extends State<MyApp> {
         favoritesList.add(candidateMeal);
       }
     });
-    print("----" + favoritesList.toString());
   }
 
   Map<String, bool> filterSelection = {
